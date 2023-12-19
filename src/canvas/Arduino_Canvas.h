6092d9f119014283f415d48078ff89c96a719029
rdekens <rdekens@internode.on.net>
2023-12-20 09:39:01 +1030

    Update Arduino_Canvas.h

--- src/canvas/Arduino_Canvas.h
+++ src/canvas/Arduino_Canvas.h
@@ -25,6 +25,7 @@ public:
   void draw16bitRGBBitmapWithTranColor(int16_t x, int16_t y, uint16_t *bitmap, uint16_t transparent_color, int16_t w, int16_t h) override;
   void draw16bitBeRGBBitmap(int16_t x, int16_t y, uint16_t *bitmap, int16_t w, int16_t h) override;
   void flush(void) override;
+  void flushWithTrans(uint16_t transparent_color);  
   void flushQuad(void);
 
   uint16_t *getFramebuffer();
